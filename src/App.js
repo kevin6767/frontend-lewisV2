396484c814312943712afa3d8549e18e51edc3bc
Kevin Eslick <eslick.kevin@yahoo.com>
2019-11-19 19:46:17 -0600

    playing around with hot reloading. On to images

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Hello World! I'm working!  hot Reloading!</h1>
+                <h1>Hello World! I'm working! Am I working?</h1>
             </div>
         )
     }
