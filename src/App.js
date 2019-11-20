707e9e071a4fd91d62e4d2c3ccc0d89d8d5f8922
Kevin Eslick <eslick.kevin@yahoo.com>
2019-11-19 20:42:28 -0600

    added image loading to webpack on to SCSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import {hot} from 'react-hot-loader';
 import './App.css';
-
+import { Jumbotron } from './components/Jumbotron.js'
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Jumbotron />
                 <h1>Hello World! I'm working! Am I working?</h1>
+               
             </div>
         )
     }
