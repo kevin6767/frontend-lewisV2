ff065eb43189ba8d50cce0c9de34062e563af2fb
Kevin Eslick <eslick.kevin@yahoo.com>
2019-11-19 19:31:53 -0600

    Adding hot loading. About to add image support and SCSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import {hot} from 'react-hot-loader';
+import './App.css';
 
-export default class App extends Component {
+class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Hello World! I'm working!</h1>
+                <h1>Hello World! I'm working!  hot Reloading!</h1>
             </div>
         )
     }
 }
+
+export default hot(module)(App);
